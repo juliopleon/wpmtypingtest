420bb950c19a1765e02f8133d9be6016d2431909
Julio Leon <juliopena143@icloud.com>
2025-01-20 17:07:36 -0700

    display text under target text

--- tutorial.py
+++ tutorial.py
@@ -11,26 +11,32 @@ def start_screen(stdscr):
     stdscr.getkey()
 
 
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+
+    for i, char in enumerate(current):
+        # start on index 0 so on, i is the char inserted
+        stdscr.addstr(0, i, char, curses.color_pair(1))
+
+
 def wpm_test(stdscr):
     target_text = "Hello world this is some text for the testing for the app!"
     current_text = []
 
     while True:
-        key = stdscr.getkey()
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text)
+        stdscr.refresh()
 
+        key = stdscr.getkey()
         # ordinal value 27 == escape
         if ord(key) == 27:
             break
-
-        current_text.append(key)
-
-        stdscr.clear()
-        stdscr.addstr(target_text)
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
-
-        stdscr.refresh()
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
 
 
 def main(stdscr):
