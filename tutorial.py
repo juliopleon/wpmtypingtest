292a1164b8ee13d5e32cced3ff8e49e568c478e1
Julio Leon <juliopena143@icloud.com>
2025-01-21 16:29:33 -0700

    wpm time

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,6 @@
 import curses
 from curses import wrapper
+import time
 
 
 def start_screen(stdscr):
@@ -29,8 +30,12 @@ def wpm_test(stdscr):
     target_text = "Hello world this is some text for the testing for the app!"
     current_text = []
     wpm = 0
+    start_time = time.time()
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
         stdscr.clear()
         display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
